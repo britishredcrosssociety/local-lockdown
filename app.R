ea41880bbdc66aa39e3159dbf6abb63b1535eaf0
Matt Thomas <matthewgthomas@gmail.com>
2020-08-02 08:55:08 +0100

    Highlight selected LAs

--- app.R
+++ app.R
@@ -18,6 +18,8 @@ la_data = read_csv("data/local authority stats.csv")
 lad = read_sf("data/Local_Authority_Districts__December_2019__Boundaries_UK_BGC.shp")
 vi = read_sf("data/vulnerability.geojson")
 
+lad = lad %>% st_transform(crs = 4326)
+
 
 # ---- UI ----
 ui <- bootstrapPage(
@@ -25,7 +27,7 @@ ui <- bootstrapPage(
     
     leafletOutput("map", width = "100%", height = "100%"),
     
-    absolutePanel(top=0, left=50, titlePanel("Local Lockdown")),
+    absolutePanel(top = 0, left = 50, titlePanel("Local Lockdown")),
     
     absolutePanel(top = 10, right = 10,
                   selectInput("lad", 
@@ -43,6 +45,25 @@ server <- function(input, output) {
         leaflet(lad, options = leafletOptions(minZoom = 5, maxZoom = 12, attributionControl = F)) %>% 
             setView(lat=54.00366, lng=-2.547855, zoom=7) %>%  # centre map on Whitendale Hanging Stones, the centre of GB: https://en.wikipedia.org/wiki/Centre_points_of_the_United_Kingdom
             addProviderTiles(providers$CartoDB.Positron)
+    })
+    
+    # ---- Change map when user selects a Local Authority ----
+    # Show data for and zoom to selected Local Authority
+    filteredLA <- reactive({
+        lad %>% filter(lad19nm == input$lad)
+    })
+    
+    filteredVI <- reactive({
+        vi %>% filter(LAD19CD == input$lad)
+    })
+    
+    pal <- colorNumeric("viridis", 1:10)
+    
+    observe({
+        leafletProxy("map", data = filteredLA()) %>%
+            clearShapes() %>%
+            addPolygons(fill = FALSE) %>% 
+            addPolygons(data = filteredVI(), fillColor = ~pal(Vulnerability.decile), fillOpacity = 0.7)
     })
 }
 

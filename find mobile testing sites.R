34f92b7b95b8afd00a3ba85628766853c832929a
Matt Thomas <matthewgthomas@gmail.com>
2020-08-10 12:16:19 +0100

    Fixed bug where step 2 of for loop was only outputting hospital ID == 1

--- find mobile testing sites.R
+++ find mobile testing sites.R
@@ -8,60 +8,61 @@ library(readxl)
 library(httr)
 library(sf)
 
-# # ---- Load Vulnerability Index ----
-# vi = read_sf("https://github.com/britishredcrosssociety/covid-19-vulnerability/raw/master/output/vulnerability-MSOA-UK.geojson")
-# 
-# vi = vi %>% 
-#   dplyr::select(Code, Socioeconomic.Vulnerability.decile)
-# 
-# 
-# # ---- Load and geocode hospitals ----
-# hospitals_eng = read_csv("data/hospitals-england.csv")
-# # hospitals_wal = read_excel("https://github.com/britishredcrosssociety/covid-19-vulnerability/raw/master/data/hospital-data/Wales%20hospital%20list.xls")
-# 
-# # Filter out 'Mental Health Hospital'
-# hospitals_eng %>% 
-#   filter(SubType != "Mental Health Hospital")
-# 
-# # download and unzip the ONS Postcode Directory (February 2020): https://geoportal.statistics.gov.uk/datasets/ons-postcode-directory-february-2020
-# GET("https://www.arcgis.com/sharing/rest/content/items/82889274464b48ae8bf3e9458588a64b/data",
-#     write_disk(tf <- tempfile(fileext = ".zip")))
-# 
-# unzip(tf, exdir = "data/postcodes")
-# unlink(tf); rm(tf)
-# 
-# # load postcode directory and keep only relevant fields - we just need MSOA
-# postcodes_raw = read_csv("data/postcodes/Data/ONSPD_FEB_2020_UK.csv")
-# postcodes = postcodes_raw %>% dplyr::select(Postcode = pcd, msoa11, oslaua)
-# 
-# # the ONS data truncates 7-character postcodes to remove spaces (e.g. CM99 1AB --> CM991AB); get rid of all spaces in both datasets to allow merging
-# # Mutate to upper case
-# postcodes <- postcodes %>% 
-#   mutate(Postcode2 = str_to_upper(Postcode),
-#          Postcode2 = str_replace_all(Postcode, " ", ""))
-# 
-# hospitals_eng <- hospitals_eng %>% 
-#   mutate(Postcode2 = str_to_upper(Postcode),
-#          Postcode2 = str_replace_all(Postcode, " ", ""))
-# 
-# # keep only hospitals in high vulnerability area
-# hospitals_eng_vi = hospitals_eng %>% 
-#   left_join(postcodes, by = "Postcode2") %>%
-#   left_join(vi, by = c("msoa11" = "Code")) %>% 
-#   
-#   filter(Socioeconomic.Vulnerability.decile >= 9)
-# 
-# # convert to spatial dataframe and save
-# hospitals_eng_vi %>% 
-#   dplyr::select(OrganisationName, Sector, ParentName, Address1, Address2, Address3, City, Postcode = Postcode.x, Website, LAD19CD = oslaua, MSOA11CD = msoa11, Longitude, Latitude) %>% 
-#   filter(!is.na(Longitude) & !is.na(Latitude)) %>% 
-#   st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>% 
-#   write_sf("data/hospitals.shp")
-# 
-# # save as .csv too
-# hospitals_eng_vi %>% 
-#   dplyr::select(OrganisationName, Sector, ParentName, Address1, Address2, Address3, City, Postcode = Postcode.x, Website, LAD19CD = oslaua, MSOA11CD = msoa11, Longitude, Latitude) %>% 
-#   write_csv("data/hospitals.csv")
+# ---- Load Vulnerability Index ----
+vi = read_sf("https://github.com/britishredcrosssociety/covid-19-vulnerability/raw/master/output/vulnerability-MSOA-UK.geojson")
+
+vi = vi %>%
+  dplyr::select(Code, Socioeconomic.Vulnerability.decile)
+
+
+# ---- Load and geocode hospitals ----
+hospitals_eng = read_csv("data/hospitals-england.csv")
+# hospitals_wal = read_excel("https://github.com/britishredcrosssociety/covid-19-vulnerability/raw/master/data/hospital-data/Wales%20hospital%20list.xls")
+
+# Filter out 'Mental Health Hospital'
+hospitals_eng %>%
+  filter(SubType != "Mental Health Hospital")
+
+# download and unzip the ONS Postcode Directory (February 2020): https://geoportal.statistics.gov.uk/datasets/ons-postcode-directory-february-2020
+GET("https://www.arcgis.com/sharing/rest/content/items/82889274464b48ae8bf3e9458588a64b/data",
+    write_disk(tf <- tempfile(fileext = ".zip")))
+
+unzip(tf, exdir = "data/postcodes")
+unlink(tf); rm(tf)
+
+# load postcode directory and keep only relevant fields - we just need MSOA
+postcodes_raw = read_csv("data/postcodes/Data/ONSPD_FEB_2020_UK.csv")
+postcodes = postcodes_raw %>% dplyr::select(Postcode = pcd, msoa11, oslaua)
+
+# the ONS data truncates 7-character postcodes to remove spaces (e.g. CM99 1AB --> CM991AB); get rid of all spaces in both datasets to allow merging
+# Mutate to upper case
+postcodes <- postcodes %>%
+  mutate(Postcode2 = str_to_upper(Postcode),
+         Postcode2 = str_replace_all(Postcode, " ", ""))
+
+hospitals_eng <- hospitals_eng %>%
+  mutate(Postcode2 = str_to_upper(Postcode),
+         Postcode2 = str_replace_all(Postcode, " ", ""))
+
+# keep only hospitals in high vulnerability area
+hospitals_eng_vi = hospitals_eng %>%
+  left_join(postcodes, by = "Postcode2") %>%
+  left_join(vi, by = c("msoa11" = "Code")) #%>%
+
+#  filter(Socioeconomic.Vulnerability.decile >= 9)
+
+# convert to spatial dataframe and save
+hospitals_eng_vi %>%
+  dplyr::select(OrganisationName, Sector, ParentName, Address1, Address2, Address3, City, Postcode = Postcode.x, Website, LAD19CD = oslaua, MSOA11CD = msoa11, Longitude, Latitude) %>%
+  filter(!is.na(Longitude) & !is.na(Latitude)) %>%
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  write_sf("data/hospitals.shp")
+
+# save as .csv too
+hospitals_eng_vi %>%
+  dplyr::select(OrganisationName, Sector, ParentName, Address1, Address2, Address3, City, Postcode = Postcode.x, Website, LAD19CD = oslaua, MSOA11CD = msoa11, Longitude, Latitude) %>%
+  write_csv("data/hospitals.csv")
+
 
 # ---- Find hospitals whose neighbouring MSOAs are highly vulnerable ----
 # Load geodata
@@ -104,11 +105,11 @@ vul_neighbours <- tibble(id = integer(),
 # 3.
 # Add ID to results table 
 
-for(id in hosp_list$id) {
+for(hosp_id in hosp_list$id) {
   
   # 1.
   row_nums <- hospitals_vi %>% 
-    filter(id == id | Socioeconomic.Vulnerability.decile >= 9) %>% 
+    filter(id == hosp_id | Socioeconomic.Vulnerability.decile >= 9) %>% 
     st_touches() %>% 
     as_tibble()  %>% 
     pull(row.id) %>% 
@@ -116,13 +117,13 @@ for(id in hosp_list$id) {
   
   # 2.
   is_id_vulnerable <- hospitals_vi %>% 
-    filter(id == id | Socioeconomic.Vulnerability.decile >= 9) %>% 
+    filter(id == hosp_id | Socioeconomic.Vulnerability.decile >= 9) %>% 
     mutate(row_num = row_number(),
            vul_neighbour = if_else(row_num %in% row_nums,
                                    TRUE,
                                    FALSE)) %>% 
     as_tibble() %>% 
-    filter(id == 1) %>% 
+    filter(id == hosp_id) %>% 
     select(id, vul_neighbour)
   
   # 3.
@@ -135,3 +136,8 @@ for(id in hosp_list$id) {
 # border a vulnerable MSOAs
 hospitals_sp <- hospitals_sp %>% 
   left_join(vul_neighbours, by = "id")
+
+# Save
+hospitals_sp %>% 
+  filter() %>% 
+  write_sf("data/hospital-test-sites.shp")
